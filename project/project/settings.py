efe6835b1e2691d8e63d6991b1daf4d23e3d1f13
Ayo-lenz <johnlenzvictor@gmail.com>
2025-04-27 10:02:38 +0100

    tested api route

--- project/project/settings.py
+++ project/project/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "school_app",
-        "USER": "root",
-        "PASSWORD": "snoopdogg",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
